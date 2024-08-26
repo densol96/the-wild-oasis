19e61b9ceaa8d55c849b720907f6f86139fa6da8
Deniss Solovjovs <deniss11sol@gmail.com>
2024-08-26 14:07:54 +0300

    NEW: File uploading code refined and all edge cases checked

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,10 @@ export async function getCabins() {
 }
 
 export async function deleteCabin(id) {
+  const throwable = new Error(
+    "Sorry, cabin could not be deleted! Try again later!"
+  );
+
   const { error: fetchError, data: fetchedData } = await supabase
     .from("cabins")
     .select()
@@ -18,14 +22,17 @@ export async function deleteCabin(id) {
     .single();
 
   if (fetchError) {
-    console.error(fetchError);
-    throw new Error("Cabins could not be deleted");
+    console.error(
+      "deleteCabin --> unable to retrieve the cabin -> ",
+      fetchError
+    );
+    throw throwable;
   }
 
   const { error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
-    console.error(error);
-    throw new Error("Cabins could not be deleted");
+    console.error("deleteCabin --> unable to delete the cabin -> ", error);
+    throw throwable;
   }
 
   if (!fetchedData.image) return;
@@ -36,27 +43,30 @@ export async function deleteCabin(id) {
       .remove([fetchedData.image.split("/").at(-1)]);
 
   if (error) {
-    console.error("Error deleting file:", errorOnDeletingImageFromStorage);
-    throw new Error("Could not delete file");
+    console.error(
+      "Error deleting the image file in the bucket:",
+      errorOnDeletingImageFromStorage
+    );
   }
-
-  console.log("File deleted successfully:", deletedImage);
 }
 
 export async function createCabin(newCabin) {
+  const throwable = new Error("Cabin could not be created");
+
+  // Could be not probided as image is not required => newCabin.image could be undefined
   const imageName = `${Math.random()}-${newCabin.image?.name}`.replaceAll(
     "/",
     ""
   );
-
   const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
   const { error, data } = await supabase
     .from("cabins")
     .insert([{ ...newCabin, image: newCabin.image ? imagePath : null }]);
+
   if (error) {
-    console.error(error);
-    throw new Error("Cabin could not be created");
+    console.error("Unable to save a cabin to DB -> ", error);
+    throw throwable;
   }
 
   if (!newCabin.image) return;
@@ -68,6 +78,7 @@ export async function createCabin(newCabin) {
   if (storageError) {
     console.log("Cabin-image could not be uploaded: ", storageError);
     deleteCabin(data.id);
+    throw throwable;
   }
 }
 
@@ -75,15 +86,21 @@ export async function updateCabin(oldCabin, newCabin) {
   let imageName = null;
   let imageNameToBeDeletedFromStorage = null;
 
+  /* COMMENT 1.1
+   * oldCabin.image format is https://supabaseUrl/pathToBucket/cabin-images/IMAGE_NAME
+   * newCabin.image is a File with meta-data (name === IMAGE_NAME) if not re-uploaded / deleted
+   */
+
   if (
-    oldCabin.image == newCabin.image || // in case remains null
-    oldCabin.image?.endsWith(newCabin.image?.name)
+    oldCabin.image == newCabin.image || // in case remains NULL (== undefined)
+    oldCabin.image?.endsWith(newCabin.image?.name) // see COMMENT 1.1 also possible that either new or old was changed to NULL => safer to use ?.
   ) {
-    // Keep it the same if the image was not re-uploaded / deleted
+    // Keep it the same if the image was not re-uploaded / deleted / changed from null
     imageName = newCabin.image?.name || null;
   } else {
+    // Else we need to upload a new image and delete IF the old one was a picture and NOT NULL
     imageName = `${Math.random()}-${newCabin.image?.name}`.replaceAll("/", "");
-    imageNameToBeDeletedFromStorage = oldCabin.image?.split("/")?.at(-1);
+    imageNameToBeDeletedFromStorage = oldCabin.image?.split("/")?.at(-1); // if null => ?. will result in undefined, if not need to extract IMAGE_NAME from full URL
   }
 
   const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
@@ -94,13 +111,21 @@ export async function updateCabin(oldCabin, newCabin) {
     .eq("id", oldCabin.id);
 
   if (error) {
-    console.error(error);
+    console.error("Unable to write changes to DB -> ", error);
     throw new Error("Cabin could not be updated");
   }
 
-  // With supabase, apparently, file overwriting in a bucker doesn't work, so I need to delete the old one
+  if (newCabin.image) {
+    const { error: duplicateImage } = await supabase.storage
+      .from("cabin-images")
+      .upload(imageName, newCabin.image);
 
-  let errorOnDeletingOldImage = false;
+    if (duplicateImage) {
+      // In case the image is the same as the last one (Math.random() + imageName remained the same => exactly the same picture) => no need to deleted it
+      imageNameToBeDeletedFromStorage = undefined;
+    }
+  }
+
   if (imageNameToBeDeletedFromStorage) {
     const { error: errorOnDeletingImageFromStorage, data: deletedImage } =
       await supabase.storage
@@ -108,32 +133,11 @@ export async function updateCabin(oldCabin, newCabin) {
         .remove([imageNameToBeDeletedFromStorage]);
 
     if (error) {
-      /*
-       * If for some reson, unable to delete the old image, at this point just log the error out to get fixed later, but since the db has already been
-       * updated with a new path to a new image, we need to make sure we actually contiue executing the code and get to uplaod new image
-       */
-      errorOnDeletingOldImage = true;
+      // If there is a technical problem with cleaning up a bucker, just log the error
       console.log(
         "Error deleting old image file:",
         errorOnDeletingImageFromStorage
       );
-    }
-  }
-
-  if (newCabin.image) {
-    const { error: storageError } = await supabase.storage
-      .from("cabin-images")
-      .upload(imageName, newCabin.image);
-
-    if (storageError) {
-      /*
-       * In case there was an error uploading the new image, but the db was already updated, need to reset to null and display the message to the user
-       */
-      await supabase
-        .from("cabins")
-        .update({ image: null })
-        .eq("id", oldCabin.id);
-      throw new Error("Picture service unavailable, try again later!");
     }
   }
 }
